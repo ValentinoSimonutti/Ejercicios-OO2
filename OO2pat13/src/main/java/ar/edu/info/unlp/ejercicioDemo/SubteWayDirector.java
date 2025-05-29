d6c5464dc1903938d8af301b8adf6294cdc3d3fe
Valentino <valentinosimonutti@gmail.com>
2025-05-29 11:16:28 -0300

    Update SubteWayDirector.java

--- OO2pat13/src/main/java/ar/edu/info/unlp/ejercicioDemo/SubteWayDirector.java
+++ OO2pat13/src/main/java/ar/edu/info/unlp/ejercicioDemo/SubteWayDirector.java
@@ -2,23 +2,11 @@ package ar.edu.info.unlp.ejercicioDemo;
 
 public class SubteWayDirector { //SubteWay
 	
-	private SandwichBuilder builder;
-	
-	public SubteWayDirector(SandwichBuilder b) {
-		this.builder= b;
-	}
-
-	public void setBuilder(SandwichBuilder b) {
-		this.builder=b;
-	}
-	
-	public Sandwich armarSandwich() {
-		
+	public Sandwich armarSandwich(SandwichBuilder builder) {
 		this.builder.construirPan();
 		this.builder.construirAderezo();
 		this.builder.construirPrincipal();
 		this.builder.construirAdicional();
 		return this.builder.getSandwich();
-		
 	}
 }
